30908fa2c0962628fae627803e94a077682ae47f
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-05-08 12:52:48 -0400

    bug fixed innew interpreter

--- SPs/kernel_interpreter.py
+++ SPs/kernel_interpreter.py
@@ -2,16 +2,15 @@ from venture.lite.psp import DeterministicPSP
 from venture.lite.value import VentureSymbol
 
 kernelList=['LIN','PER','SE','WN']
-
+#             0     1     2    3
 
 
 class GrammarInterpreter(DeterministicPSP):
 
     def simulate(self,args):
-        
         K = args.operandValues[0]
         unsorted_string = K.stuff['name']
-        unsorted_string = unsorted_string.replace("x","")
+        unsorted_string = args.operar.replace("x","")
         for i in range(len(kernelList)):
             unsorted_string=unsorted_string.replace(kernelList[i],str(i))
         products = unsorted_string.split('+')
@@ -19,12 +18,29 @@ class GrammarInterpreter(DeterministicPSP):
         products.sort(key = lambda s: len(s))
         outstring=""
         for j in range(len(products)):
-            currentProduct= ''.join(sorted(products[j]))
-            currentProduct= "x".join(currentProduct)
-            outstring+=currentProduct
+            products[j]= ''.join(sorted(products[j]))
+            print(products[j])
+            while ("22" in products[j]):
+                products[j]=products[j].replace("22","2")
+            while (("13" in products[j]) or ("23" in products[j]) ):
+                products[j]=products[j].replace("13","3")
+                products[j]=products[j].replace("23","3")
+            products[j]= "x".join(products[j])
+
+        products.sort()
+        products.sort(key = lambda s: len(s))
+        for j in range(len(products)):
+            outstring+=products[j]
             if j<(len(products)-1):
                 outstring+="+"
+
+        while ("0+0+" in outstring):
+            outstring=outstring.replace("0+0+","0+")
+        if outstring == "0+0":
+            outstring = "0"
         for i in range(len(kernelList)):
             outstring= outstring.replace(str(i),kernelList[i])
+
+
         return   VentureSymbol(outstring)  
     
