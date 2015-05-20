7420e818b17cfe6970cca8dd71121674973fe184
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-05-19 23:28:58 -0400

    registered simple compound gps and added them to .ini

--- Experiments/all_registered_models.py
+++ Experiments/all_registered_models.py
@@ -14,5 +14,11 @@ lin_gp = LIN_Venture_GP_Model()
 registered_models['venture-gp-lin']=lin_gp
 
 
+from models.lin_plus_per_gp_model import LIN_P_PER_Venture_GP_Model
+lin_p_per_gp = LIN_P_PER_Venture_GP_Model()
+registered_models['venture-gp-lin-p-per']=lin_p_per_gp
 
 
+from models.lin_times_per_gp_model import LIN_T_PER_Venture_GP_Model
+lin_t_per_gp = LIN_T_PER_Venture_GP_Model()
+registered_models['venture-gp-lin-t-per']=lin_t_per_gp
--- Experiments/models/lin_times_per_gp_model.py
+++ Experiments/models/lin_times_per_gp_model.py
@@ -3,16 +3,16 @@ __author__ = 'ulli'
 from venture_gp_model import Venture_GP_model
 from venture.lite.function import VentureFunction
 import venture.lite.types as t
-from covFunctions import makeLinear,makePeriodic,makeLiftedAdd
-
-class LIN_P_PER_Venture_GP_Model(Venture_GP_model):
+from covFunctions import makeLinear,makePeriodic,makeLiftedMult
+import numpy as np
+class LIN_T_PER_Venture_GP_Model(Venture_GP_model):
     #def __init__(self):
     #super(SE_GP_Model, self).__init__()
     def make_gp(self, ripl):
         ripl.assume('make_lin',VentureFunction(makeLinear,[t.NumberType()], t.AnyType("VentureFunction")))
         ripl.assume('make_per',VentureFunction(makePeriodic,[t.NumberType(), t.NumberType(), t.NumberType()], t.AnyType("VentureFunction")))
 
-        ripl.assume("func_plus", makeLiftedAdd(lambda x1, x2: x1+x2))
+        ripl.assume("func_times", makeLiftedMult(lambda x1, x2:np.multiply(x1,x2)))
 
         ripl.assume('gp',"""(tag (quote parameter) 0
                                 (make_gp_part_der zero
