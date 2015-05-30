6bc5fca71aee420aaca38643768a429624ec847e
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-05-30 12:46:49 -0400

    full preprocessing pipeline that can be called from command line on PC's bigger than my laptop. Will genereate very large dfs

--- Experiments/generate_dfs.py
+++ Experiments/generate_dfs.py
@@ -1,31 +1,42 @@
 import sys
 from plotting import *
 import pandas as pd
+import os
+
 structure_posterior = False
 mean_residuals = False
 median_residuals = False
 last_n_residuals = False
+overwrite_existing = False
 
 for i in range(1,len(sys.argv)):
             if str(sys.argv[i])=="-sp": # structure posterior
                 structure_posterior= True
-            if str(sys.argv[i])=="--median": # structure posterior
+            if str(sys.argv[i])=="--median": # median residuals
                 median_residuals= True
-            if str(sys.argv[i])=="--mean": # structure posterior
+            if str(sys.argv[i])=="--mean": # mean residuasl
                 mean_residuals= True
-            if str(sys.argv[i])=="--last": # structure posterior
+            if str(sys.argv[i])=="--last": # last n residuals
                 last_n_residuals= True
             if str(sys.argv[i])=="-d":
                 date_experiment= str(sys.argv[i+1])
             if str(sys.argv[i])=="-f":
                 experiment_ini_file= str(sys.argv[i+1])
+            if str(sys.argv[i])=="--delete": # last n residuals
+                overwrite_existing = True
+
+if overwrite_existing and last_n_residuals:
+    print("Warning - cannot overwrite last n residuals - needs to be deleted by hand")
 
 
 if structure_posterior:
-    df =get_posterior_structure(date_experiment,experiment_ini_file)
+    df = get_posterior_structure(date_experiment, experiment_ini_file,overwrite_existing)
 if mean_residuals:
-    df =get_dataFrame(date_experiment,experiment_ini_file)
+    df = get_dataFrame(date_experiment,experiment_ini_file,overwrite_existing)
 if median_residuals:
-    df =get_dataFrame_median(date_experiment,experiment_ini_file)
+    df = get_dataFrame_median(date_experiment,experiment_ini_file,overwrite_existing)
 if last_n_residuals:
-    df =get_last_n_residuals(date_experiment,experiment_ini_file)
+   os.system("preprocess_residuals.py -d "+date_experiment+" -f "+experiment_ini_file)
+
+if overwrite_existing and last_n_residuals:
+    print("Warning - cannot overwrite last n residuals - needs to be deleted by hand")
