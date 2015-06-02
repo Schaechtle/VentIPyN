b9a3a48649dd7bb41e96a864f92becab46c8564b
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-06-02 18:28:37 -0400

    added the simple load

--- Experiments/generate_dfs.py
+++ Experiments/generate_dfs.py
@@ -8,6 +8,8 @@ mean_residuals = False
 median_residuals = False
 last_n_residuals = False
 overwrite_existing = False
+simple_df_load = False
+
 
 for i in range(1,len(sys.argv)):
             if str(sys.argv[i])=="-sp": # structure posterior
@@ -22,6 +24,9 @@ for i in range(1,len(sys.argv)):
                 date_experiment= str(sys.argv[i+1])
             if str(sys.argv[i])=="-f":
                 experiment_ini_file= str(sys.argv[i+1])
+            if str(sys.argv[i])=="--simple":
+                str_2_start= str(sys.argv[i+1])
+                simple_df_load=True
             if str(sys.argv[i])=="--delete": # last n residuals
                 overwrite_existing = True
 
@@ -37,6 +42,7 @@ if median_residuals:
     _ = get_dataFrame_median(date_experiment,experiment_ini_file,overwrite_existing)
 if last_n_residuals:
    os.system("preprocess_residuals.py -d "+date_experiment+" -f "+experiment_ini_file)
-
 if overwrite_existing and last_n_residuals:
     print("Warning - cannot overwrite last n residuals - needs to be deleted by hand")
+if simple_df_load:
+    simple_load(date_experiment,str_2_start)
