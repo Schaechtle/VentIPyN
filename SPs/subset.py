f18f0eb575b661b7a3de51d94bae52ba16df1f56
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-06-07 01:04:18 -0400

    subset with probability of size as arg

--- SPs/subset.py
+++ SPs/subset.py
@@ -14,18 +14,17 @@ class Subset(RandomPSP):
   def isRandom(self): return True
   def simulate(self,args):
     list_of_items= args.operandValues[0]
-    subset_size = args.operandValues[1]
+    p = args.operandValues[1]
+    subset_size = np.random.multinomial(1, p).argmax()+1
     assert subset_size <= len(list_of_items)
     return random.sample(list_of_items,subset_size)
 
 
   def logDensity(self,val,args):
     list_of_items= args.operandValues[0]
-    subset_size = args.operandValues[1]
-    assert subset_size <= len(list_of_items)
+    p = args.operandValues[1]
     #import ipdb;ipdb.set_trace()
-    assert len(val)==subset_size, 'needs to be block sampled with the size of the subset!'
-    return np.log(sc.factorial(subset_size)/sc.factorial(len(list_of_items)))
+    return np.log(sc.factorial(len(val))/sc.factorial(len(list_of_items))) +np.log(p[len(val)-1])
 
 
 
