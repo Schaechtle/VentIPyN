b9ef24694b3eedf1df4902ecb68c1e7c85f163b0
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-06-06 20:37:09 -0400

    subset SP and  compound test IPy notebook

---
+++ SPs/subset.py
@@ -0,0 +1,44 @@
+
+import random
+import numpy as np
+
+
+from venture.lite.psp import RandomPSP
+import scipy
+import scipy.misc as sc
+
+
+
+
+
+
+
+class Subset(RandomPSP):
+
+  def isRandom(self): return True
+  def simulate(self,args):
+    list_of_items= args.operandValues[0]
+    subset_size = args.operandValues[1]
+    assert subset_size <= len(list_of_items)
+    return random.sample(list_of_items,subset_size)
+
+
+
+  def logDensity(self,val,args):
+    list_of_items= args.operandValues[0]
+    subset_size = args.operandValues[1]
+    assert subset_size <= len(list_of_items)
+    #import ipdb;ipdb.set_trace()
+    assert len(val)==subset_size, 'needs to be block sampled with the size of the subset!'
+    return np.log(sc.factorial(subset_size)/sc.factorial(len(list_of_items)))
+
+
+
+
+
+
+
+  
+  
+  
+  
