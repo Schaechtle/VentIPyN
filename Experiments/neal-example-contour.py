d41a25ab26b896ded14c241e496c1c9ca2f58cb6
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-06-11 10:18:35 -0400

    s

--- Experiments/neal-example-contour.py
+++ Experiments/neal-example-contour.py
@@ -175,11 +175,11 @@ def plot_hyper(n_iteration):
 def plot_contours(df,name,n_iteration):
 
     joint_grid_plot("l","sigma",df,name,n_iteration)
-    joint_grid_plot("l","sf",df,name,n_iteration)
-    joint_grid_plot("sf","sigma",df,name,n_iteration)
+    #joint_grid_plot("l","sf",df,name,n_iteration)
+    #joint_grid_plot("sf","sigma",df,name,n_iteration)
     joint_grid_plot("l","sigma",df,name,n_iteration,False)
-    joint_grid_plot("l","sf",df,name,n_iteration,False)
-    joint_grid_plot("sf","sigma",df,name,n_iteration,False)
+    #joint_grid_plot("l","sf",df,name,n_iteration,False)
+    #joint_grid_plot("sf","sigma",df,name,n_iteration,False)
 
 
 
