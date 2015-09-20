8e137ef3281ab2f2c4f4921b9caa880743ee7364
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-09-20 21:13:00 +0100

    defined hyper-parameters separately, instead of with a procedure

--- Experiments/all_registered_models.py
+++ Experiments/all_registered_models.py
@@ -49,13 +49,10 @@ registered_models['venture-cov-learning-smart-simple']=venture_cov_learning_smar
 from models.cov_smart_rq import Grammar_Venture_GP_smart_rq
 venture_cov_learning_smart_rq = Grammar_Venture_GP_smart_rq()
 registered_models['venture-cov-learning-smart-rq']=venture_cov_learning_smart_rq
-<<<<<<< HEAD
 
 
 
+from models.cov_smart_simple2 import Grammar_Venture_GP_smart_simple2
+venture_cov_learning_smart_simple2 = Grammar_Venture_GP_smart_simple2()
+registered_models['venture-cov-learning-smart-simple2']=venture_cov_learning_smart_simple2
 
-from models.cov_by_grammar_gp_simpleSELINPER import Grammar_Venture_GP_model_simpleSELINPER
-venture_cov_simpleSELINPER = Grammar_Venture_GP_model_simpleSELINPER()
-registered_models['venture_cov_simple_selinper']=venture_cov_simpleSELINPER
-=======
->>>>>>> 0c478d2a61c8519f3134e686e012a5c4663a4cd9
