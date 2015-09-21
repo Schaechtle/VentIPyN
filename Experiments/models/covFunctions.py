5b19ae49738768043f14baabc3904da94c7e874d
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-09-21 22:08:37 +0100

    removed comments

--- Experiments/models/covFunctions.py
+++ Experiments/models/covFunctions.py
@@ -26,51 +26,49 @@ def makeConstFunc(c):
 def array(xs):
   return t.VentureArrayUnboxed(np.array(xs),  t.NumberType())
 
-def const(sf):
-    sf2 = np.exp(sf)         # s2
+def const(sf2):         # s2
     def f(x1,  x2=None):
         if x2 is None: # self-covariances for test cases           # self covariances for the test cases
             nn,D = x1.shape
-            A = sf2 * np.ones((nn,1))
+            A = sf2**2 * np.ones((D,D))
         else:            # compute covariance matix for dataset x
             n,D = x1.shape
-            A = sf2 * np.ones((n,n)) + np.eye(n)*1e-10
+            A = sf2**2 * np.ones((x1.shape[0],x2.shape[0]))
         return A
     return f
 
-def const_der(sf):
-    sf2 = np.exp(sf)         # s2
+
+def const_der(sf2):
     def f(x1,  x2=None):
         n,D = x1.shape
         A = sf2 * np.ones((n,n))
         return 2. * A
     return f
+
+
+
+
 def makeConst(sf):
-  return VentureFunction(const(sf), sp_type=covfunctionType,derivatives={0:const_der(sf)},name="C",parameter=[sf])
+  return VentureFunction(const(sf), sp_type=covfunctionType,name="C",derivatives={0:const_der(sf)},parameter=[sf])
 
 
 def squared_exponential(sf, l):
-  sf = np.exp(sf)
-  l = np.exp(l)
-  def f(x1,  x2=None):
+ def f(x1,  x2=None):
     if x2 is None: # self-covariances for test cases
         nn,D = x1.shape
         A = np.zeros((nn,1))
     else:    
         A = spdist.cdist(x1/l,x2/l,'sqeuclidean')
-    return sf * np.exp(-0.5*A)
-  return f
+    return sf**2 * np.exp(-0.5*A)
+ return f
+
 # gradient
 def squared_exponential_der_l(sf, l):
-  sf = np.exp(sf)
-  l = np.exp(l)
   def f(x1, x2):
     A = spdist.cdist(x1/l,x2/l,'sqeuclidean')
-    return sf * np.exp(-0.5*A) * A
+    return sf**2 * np.exp(-0.5*A) * A
   return f
 def squared_exponential_der_sf(sf, l):
-  sf = np.exp(sf)
-  l = np.exp(l)
   def f(x1, x2):
     A = spdist.cdist(x1/l,x2/l,'sqeuclidean')
     return  2. * sf * np.exp(-0.5*A)
@@ -88,19 +86,17 @@ def makeSquaredExponential(sf, l):
 # In[7]:
 
 def periodic(l,p,sf):
-  sf = np.exp(2.*sf)
-  p = np.exp(p)
-  l = np.exp(l)
   def f(x1, x2=None):
     if x2 is None: # self-covariances for test cases
         nn,D = x1.shape
         A = np.zeros((nn,1))
     else:
         A = np.sqrt(spdist.cdist(x1,x2,'sqeuclidean'))
+    #import ipdb;ipdb.set_trace()
     A = np.pi*A/p
-    A = np.sin(A)/l
-    A = A * A
-    A = sf *np.exp(-2.*A)
+    A = np.sin(A)/(l)
+    A = A*A
+    A = sf**2 *np.exp(-2.*A)
     return A
   return f
 
@@ -110,10 +106,8 @@ def periodic(l,p,sf):
 # In[8]:
 
 def periodic_der_l(l,p,sf):
-  sf = np.exp(2.*sf)
-  p = np.exp(p)
-  l = np.exp(l)
-  def f(x1, x2):    
+  sf = 2.*sf
+  def f(x1, x2):
     A = np.sqrt(spdist.cdist(x1,x2,'sqeuclidean'))
     A = np.pi*A/p
     A = np.sin(A)/l
@@ -122,9 +116,7 @@ def periodic_der_l(l,p,sf):
     return A
   return f
 def periodic_der_p(l,p,sf):
-  sf = np.exp(2.*sf)
-  p = np.exp(p)
-  l = np.exp(l)
+  sf = 2.*sf
   def f(x1, x2):
     A = np.sqrt(spdist.cdist(x1,x2,'sqeuclidean'))
     A = np.pi*A/p
@@ -133,9 +125,7 @@ def periodic_der_p(l,p,sf):
     return A
   return f
 def periodic_der_sf(l,p,sf):
-  sf = np.exp(2.*sf)
-  p = np.exp(p)
-  l = np.exp(l)
+  sf = 2.*sf
   def f(x1, x2):
     A = np.sqrt(spdist.cdist(x1,x2,'sqeuclidean'))
     A = np.pi*A/p
@@ -153,19 +143,17 @@ def makePeriodic(l,p,sf):
 
 # In[9]:
 
-def linear(sf):
-  sf = np.exp(sf)
+def linear(sf): # ToDo
   def f(x1,  x2=None):
     if x2 is None: # self-covariances for test cases
         nn,D = x1.shape
         A = np.dot(x1,x1.T)
     else:
-        A = np.dot(x1,x2.T) + 1e-10    # required for numerical accuracy
-    return sf * A
+        A =  np.dot(x1,x2.T) + 1e-10    # required for numerical accuracy
+    return sf**2* A
   return f
 # gradient
 def linear_der_sf(sf):
-  sf = np.exp(sf)
   def f(x1, x2):
     A = np.dot(x1,x2.T)
     return 2 * sf * A
@@ -181,7 +169,6 @@ def makeLinear(sf):
 # In[10]:
 
 def noise(s):
-  sf = np.exp(2.*s)
   def f(x1, x2=None):
     if x2 is None: # self-covariances for test cases
         nn,D = x1.shape
@@ -191,12 +178,11 @@ def noise(s):
         M = spdist.cdist(x1, x2, 'sqeuclidean')
         A = np.zeros_like(M,dtype=np.float)
         A[M < tol] = 1.
-    A = s*A
+    A = s**2 *A
     return A
   return f
 # gradient
 def noise_der(s):
-  sf = np.exp(2. * s)
   def f(x1, x2):
     tol = 1.e-9                       # Tolerance for declaring two vectors "equal"
     M = spdist.cdist(x1, x2, 'sqeuclidean')
@@ -210,45 +196,33 @@ def makeNoise(s):
   return VentureFunction(noise(s), sp_type=covfunctionType,derivatives={0:noise_der(s)},name="WN",parameter=[s])
 
 
-def rationalQuadratic(log_ell,log_sf2,log_alpha):
+def rationalQuadratic(ell,sf2,alpha):
     def f(x1, x2=None):
-        ell   = np.exp(log_ell)       # characteristic length scale
-        sf2   = np.exp(2.*log_sf2)    # signal variance
-        alpha = np.exp(log_alpha)
         if x2 is None:           # self covariances for the test cases
             nn,D = x1.shape
             D2 = np.zeros((nn,1))
         else:             # compute covariance between data sets x and z
             D2 = spdist.cdist(x1/ell, x2/ell, 'sqeuclidean')
-        A = sf2 * ( ( 1.0 + 0.5*D2/alpha )**(-alpha) )
+        A = sf2**2 * ( ( 1.0 + 0.5*D2/alpha )**(-alpha) )
         return A
     return f
 
-def rationalQuadratic_der_l(log_ell,log_sf2,log_alpha):
+def rationalQuadratic_der_l(ell,sf2,alpha):
     def f(x1, x2=None):
-        ell   = np.exp(log_ell)       # characteristic length scale
-        sf2   = np.exp(2.*log_sf2)    # signal variance
-        alpha = np.exp(log_alpha)
         D2 = spdist.cdist(x1/ell, x2/ell, 'sqeuclidean')
         A = sf2 * ( 1.0 + 0.5*D2/alpha )**(-alpha-1) * D2
         return A
     return f
 
-def rationalQuadratic_der_sf2(log_ell,log_sf2,log_alpha):
+def rationalQuadratic_der_sf2(ell,sf2,alpha):
     def f(x1, x2=None):
-        ell   = np.exp(log_ell)       # characteristic length scale
-        sf2   = np.exp(2.*log_sf2)    # signal variance
-        alpha = np.exp(log_alpha)
         D2 = spdist.cdist(x1/ell, x2/ell, 'sqeuclidean')
         A = 2.* sf2 * ( ( 1.0 + 0.5*D2/alpha )**(-alpha) )
         return A
     return f
 
-def rationalQuadratic_der_alpha(log_ell,log_sf2,log_alpha):
+def rationalQuadratic_der_alpha(ell,sf2,alpha):
     def f(x1, x2=None):
-        ell   = np.exp(log_ell)       # characteristic length scale
-        sf2   = np.exp(2.*log_sf2)    # signal variance
-        alpha = np.exp(log_alpha)
         D2 = spdist.cdist(x1/ell, x2/ell, 'sqeuclidean')
         K = ( 1.0 + 0.5*D2/alpha )
         A = sf2 * K**(-alpha) * (0.5*D2/K - alpha*np.log(K) )
@@ -258,6 +232,30 @@ def rationalQuadratic_der_alpha(log_ell,log_sf2,log_alpha):
 def makeRQ(l,sf,alpha):
   return VentureFunction(rationalQuadratic(l,sf,alpha), sp_type=covfunctionType,derivatives={0:rationalQuadratic_der_l(l,sf,alpha),1:rationalQuadratic_der_sf2(l,sf,alpha),2:rationalQuadratic_der_alpha(l,sf,alpha)},name="RQ",parameter=[l,sf,alpha])
 
+def changePoint(steepness,center):
+    def f(x1,x2=None):
+        sumx1 = x1-center
+        #import  ipdb;ipdb.set_trace()
+        if x2 is None:
+            n,D = x1.shape
+            sx = 1./(1+np.exp(-x1))
+            sz=sx
+            sumz=sumx1
+            A = np.dot(sx,sz.T)
+            return A
+        x1= sumx1*steepness
+        sumx2 = x2-center
+        x2= sumx2*steepness
+        sx = 1./(1+np.exp(-x1))
+        sz = 1./(1+np.exp(-x2)) # ToDo: why do I need to index here???
+
+        A = np.dot(sx,sz.T)	
+        return A
+    return f
+
+
+def makeCP(log_steepness,log_center):
+   return VentureFunction(changePoint(log_steepness,log_center), sp_type=covfunctionType,name="CP",derivatives={},parameter=[log_steepness,log_center])
 
 
 #### Binary Operators
@@ -269,6 +267,8 @@ def lift_binary(op):
   return lifted
 
 
+
+
 liftedBinaryType = SPType([t.AnyType(), t.AnyType()], t.AnyType())
 
 def makeLiftedAdd(op):
@@ -277,6 +277,7 @@ def makeLiftedAdd(op):
     sp_type = f1.sp_type
     assert(f2.sp_type == sp_type)
     der={}
+    i = 0 #TODO delete this, only here for stubby covfunctions where der is not yet implemented
     for i in range(len(f1.stuff['derivatives'])):
         der[i]=f1.stuff['derivatives'][i]
     for j in range(len(f2.stuff['derivatives'])):
@@ -290,10 +291,17 @@ def makeLiftedMult(op):
     sp_type = f1.sp_type
     assert(f2.sp_type == sp_type)
     der={}
+    i = 0 #TODO delete this, only here for stubby covfunctions where der is not yet implemented
     for i in range(len(f1.stuff['derivatives'])):
         der[i]= lambda *xs: np.dot(f1.stuff['derivatives'][i](*xs),f2.f(*xs))
     for j in range(len(f2.stuff['derivatives'])):
         der[i+1+j]= lambda *xs: np.dot(f2.stuff['derivatives'][j](*xs),f1.f(*xs))
     return VentureFunction(lifted_op(f1,f2), sp_type=sp_type,derivatives=der,name=f1.stuff['name']+"x"+f2.stuff['name'])
   return VentureFunction(wrapped, sp_type=liftedBinaryType)
+
+def lift_change_point(op):
+  def lifted(f1, f2):
+    return lambda x1,x2: op(f1(x1,x2), f2(x1,x2))
+  return lifted
+
 
